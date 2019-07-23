b60de04852cec18aaa34694eb5259e00d363fb09
Soumya Narayanan <soumya.9988@gmail.com>
2019-07-23 17:05:13 -0500

    ML Udemy

---
+++ ml_udemy/apriori.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 22 16:49:09 2019
+
+@author: vedhoos
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the data set
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header=None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+
+# Training apriori on the dataset
+from apyori import apriori
+
+rules = apriori(transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2)
+print(rules)
+
